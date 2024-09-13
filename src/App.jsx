4d5709689f78ccd75212e773ef4cc1bb0f13f7df
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-09-13 21:49:04 +0700

    Refactor App component and remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
-
 import * as Sentry from "@sentry/react";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
